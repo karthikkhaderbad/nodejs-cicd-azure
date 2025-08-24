1e7b468b194b93604dbfd3b558370302accc5656
karthikkhaderbad <karthikkhaderbad@gmail.com>
2025-08-24 15:37:54 +0530

    added  eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module"
+    },
+    rules: {
+      // Example rule
+      "no-unused-vars": "warn"
+    }
+  }
+];
